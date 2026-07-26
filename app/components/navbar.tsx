8ff7ae05dd0718134fb4569e10ef20d6d57c4c48
Raxon Suri <raxonsuriperez@gmail.com>
2026-07-25 20:11:02 -0400

    Navigation bar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,8 +1,17 @@
+import { getSession } from "@/lib/auth";
+import Link from "next/link";
 
-export default function NavBar() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default async function NavBar() {
+  const session = await getSession();
+
+  if (!session) {
+    return (
+      <nav className="flex justify-center gap-20 py-5 bg-[#292828] rounded-b-lg">
+        <Link href="/sign-in" className="hover:underline">Sign-In</Link>
+        <Link href="/sign-up" className="hover:underline">Sign-Up</Link>
+      </nav>
+    );
+  }
+
+  return <div></div>;
 }
