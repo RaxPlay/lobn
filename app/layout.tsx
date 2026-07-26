e52911756025da34f798f97fd0ebe652d650b834
Raxon Suri <raxonsuriperez@gmail.com>
2026-07-25 20:10:45 -0400

    layout and "/" route set-up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
+
 import "./globals.css";
+import NavBar from "./components/navbar";
 
 export const metadata: Metadata = {
   title: "Kanban Board",
@@ -16,7 +18,13 @@ export default function RootLayout({
       lang="en"
       className="h-full antialiased"
     >
-      <body className="min-h-full flex flex-col">{children}</body>  
+      <body className="min-h-full flex flex-col">
+        <header>
+          <NavBar></NavBar>
+        </header>
+
+        {children}
+      </body>  
     </html>
   );
 }
