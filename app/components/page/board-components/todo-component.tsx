a1160495e0a913a0c2e1a492c7b407e9bf90c846
Raxon Suri <121737678+RaxPlay@users.noreply.github.com>
2026-07-28 23:34:20 -0400

    Styled tasks

--- app/components/page/board-components/todo-component.tsx
+++ app/components/page/board-components/todo-component.tsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import { DisplayTasks } from './board-client-main-page'
+import { FaTrash } from 'react-icons/fa';
 
 interface Props {
   displayTasks: DisplayTasks[];
 }
 
 export default function Todo({ displayTasks }: Props) {
-
-
   return (
     <div id="board-element">
       <h2>Todo</h2>
 
-      {displayTasks.map((task) => (
-        <div key={task.taskId}>
+      <div className='p-2 flex flex-col gap-2'>
+      {displayTasks.map((task, index) => (
+        <div key={index} id='task'>
           {task.taskContent}
         </div>
       ))}
+
+      </div>
     </div>
   )
 }
